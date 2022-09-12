eb2492199da7d3a28a3086533ec74b416a277fa6
David Bau <david.bau@gmail.com>
2022-09-12 09:30:45 -0400

    Add LossSurfaceWidget.

--- cs7150/__init__.py
+++ cs7150/__init__.py
@@ -1,3 +1,4 @@
 from .mpneuron import McCulloughPittsNeuron, MPNeuron, Sign, Output
 from .mlpwidget import MLPHistoryWidget
+from .losswidget import LossSurfaceWidget
 from .seed import seeded_net, non_linearly_separable_data
